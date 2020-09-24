59398447327c280501a855dc54622595c4f608e3
ivan <ivan@arqios.com>
2020-09-23 23:19:16 -0600

    Se modifico la estructura del proyecto, se agrego un nuevo componente

---
+++ src/components/ComponentTypeHouse.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ComponentTypeHouse extends React.Component{
+    render(){
+        return(
+            <h1>Type house</h1>
+        );
+    }
+}
+export default ComponentTypeHouse;
