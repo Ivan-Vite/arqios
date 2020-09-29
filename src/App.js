8e397ba8aff459481fc77a8cb839c7874bf4ee56
OyuFuentes <oyu.fuentes@gmail.com>
2020-09-28 22:51:26 -0500

    split header and navbar top

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import logo from './assets/images/logo.svg';
 import './assets/css/App.css';
 
 import ComponentTypeHouse from './components/ComponentTypeHouse';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-		<h1>ARQIOS</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reloa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <section className="componentes">
           <ComponentTypeHouse/>
         </section>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+// import logo from '../assets/images/logo.svg';
 
 function Header() {
   return (
-    <nav>
-      <h1>Header</h1>
-    </nav>
+    <header className="App-header">
+    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+    <h1>Tu Futura casa es inteligente</h1>
+    <p>Llegamos a innovar los bienes raíces en México para hacer tu experiencia de compra increíble como tu futura casa</p>
+    
+  </header>
   );
 }
 export default Header;
