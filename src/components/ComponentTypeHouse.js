96ddd1e602ce774d6929fd0a9d6138f188d9bb4f
ivan <ivan@arqios.com>
2020-10-01 18:12:04 -0600

    Se agregaron clases de estilo

--- src/components/ComponentTypeHouse.js
+++ src/components/ComponentTypeHouse.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import '../assets/css/typeHouse.css'
 
-
 class ComponentTypeHouse extends Component {
 	constructor(props) {
 		super(props);
@@ -32,18 +31,18 @@ class ComponentTypeHouse extends Component {
 			return <div>Loading...</div>;
 		} else {
 			return (
-				<section class='TypeHouse' >
+				<section className='section3'>
 				<div>
 					<p> Más que un hogar </p>
+					<h1 className='descriptionP'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl egestas elementum pretium turpis mattis mi a vivamus laoreet. Ut suspendisse aliquet vulputate nibh habitant sed tortor. Sit gravida </h1>
 					{dataSource.Items.map(item => (
-						<div className="boxes" key={item.idHouse}>
+						<section key={item.idHouse}>
 							<h1>{item.detail.name}</h1>
 							<h1>{item.detail.status}</h1>
 							<img alt={item.detail.name} src={item.detail.img} />
-							<h1>{item.detail.img}</h1>
 							<h1>{item.detail.description}</h1>
 							<h1>{item.detail.streetAddress}</h1>
-						</div>
+						</section>
 					))}
 				</div>
 				</section>
