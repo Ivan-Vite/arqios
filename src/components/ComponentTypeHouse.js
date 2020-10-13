d880a84e22aa2e8c6042fbbff4b5e78f43c5be4c
Nestor Ivan Becerril Vite <nestor.becerril@neoris.com>
2020-10-13 11:16:30 -0700

    Se agrego css a la seccion de casas

--- src/components/ComponentTypeHouse.js
+++ src/components/ComponentTypeHouse.js
@@ -1,66 +1,8 @@
 import React, { Component } from 'react';
 import '../assets/css/typeHouse.css'
 import styled from "@emotion/styled/macro";
-
-const Hover = styled.div({
-  opacity: 0,
-  transition: "opacity 350ms ease",
-});
-
-const DisplayOver = styled.div({
-  height: "100%",
-  left: "0",
-  position: "absolute",
-  top: "0",
-  width: "100%",
-  zIndex: 2,
-  transition: "background-color 350ms ease",
-  backgroundColor: "transparent",
-  padding: "20px 20px 0 20px",
-  boxSizing: "border-box",
-});
-
-const BigTitle = styled.h2({
-  textTransform: "uppercase",
-  fontFamily: "Helvetica",
-});
-
-const SubTitle = styled.h4({
-  fontFamily: "Helvetica",
-  transform: "translate3d(0,50px,0)",
-  transition: "transform 350ms ease",
-});
-
-const Paragraph = styled.p({
-  transform: "translate3d(0,50px,0)",
-  transition: "transform 350ms ease",
-});
-
-const Background = styled.div({
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  color: "#FFF",
-  position: "relative",
-  width: "500px",
-  height: "350px",
-  cursor: "pointer",
-  backgroundImage: "url(/bg.jpg)",
-  [`:hover ${DisplayOver}`]: {
-    backgroundColor: "rgba(0,0,0,.5)",
-  },
-  [`:hover ${SubTitle}, :hover ${Paragraph}`]: {
-    transform: "translate3d(0,0,0)",
-  },
-  [`:hover ${Hover}`]: {
-    opacity: 1,
-  },
-});
-
-const CTA = styled.a({
-  position: "absolute",
-  bottom: "20px",
-  left: "20px",
-});
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 
 class ComponentTypeHouse extends Component {
 	constructor(props) {
@@ -92,40 +34,22 @@ class ComponentTypeHouse extends Component {
 			return <div>Loading...</div>;
 		} else {
 			return (
-				<section className='section3'>
-				<div>
-					<h1> Más que un hogar </h1>
-					<h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl egestas elementum pretium turpis mattis mi a vivamus laoreet. Ut suspendisse aliquet vulputate nibh habitant sed tortor. Sit gravida </h2>
-					{dataSource.Items.map(item => (
-						<div className='divHouse' key={item.idHouse}>
-							<h1>{item.detail.streetAddress}</h1>
-							<h2>{item.detail.status}</h2>
-							<hr className='new1'/>
-							<canvas/>
-							<rect id="rect"/>
-							<h3>{item.detail.name}</h3>
-							<img alt={item.detail.name} src={item.detail.img} />
-							<h3>{item.detail.description}</h3>
-							
+				<div className='section3'>
+					<div class="row">
+						<div class="col-md-6 offset-md-3">
+							<h1> Más que un hogar </h1>
+							<h2> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nisl egestas elementum pretium turpis mattis mi a vivamus laoreet. Ut suspendisse aliquet vulputate nibh habitant sed tortor. Sit gravida </h2>
 						</div>
-						
+					</div>
+				<CardDeck>
+					{dataSource.Items.map(item => (
+						<Card key={item.idHouse}>
+							<Card.Img variant="top" src={item.detail.img} />
+							<Card.Title>{item.detail.streetAddress}</Card.Title>			
+						</Card>						
 					))}
+				</CardDeck>
 				</div>
-				<div className="App">
-				<Background>
-				<DisplayOver>
-					<BigTitle>Really Cool Title!</BigTitle>
-					<Hover>
-					<SubTitle>You could vacation here!</SubTitle>
-					<Paragraph>
-						More description about this really cool random desert photo from unsplash!
-					</Paragraph>
-					<CTA>View More +</CTA>
-					</Hover>
-				</DisplayOver>
-				</Background>
-      </div>
-				</section>
 			);
 		}
 	}
