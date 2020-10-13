857aa48e92605344fd1feedad30b3fde02866d44
David Vargas <david@arqios.com>
2020-10-13 18:19:10 -0500

    Add video background
    App-header updated: video asset, styles and html in component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,27 @@
 import React from "react";
-import NavbarComponent from '../components/NavbarComponent';
+import NavbarComponent from "../components/NavbarComponent";
+import video_bg from "../assets/images/Hero arqios.mp4";
+import { video } from "react-bootstrap";
 
 function Header() {
-  return (    
-    <header className="App-header" >
-      <NavbarComponent/>
-      <div className="TextHeader d-flex justify-content-center align-items-center">
-        <div className="text-center col-sm-3">
-          <h1>Tu futura casa es inteligente</h1>
+  return (
+    <header className="App-header">
+      <NavbarComponent />
+      <div className="overlay"></div>
+      <video
+        playsinline="playsinline"
+        autoplay="autoplay"
+        muted="muted"
+        loop="loop"
+      >
+        <source src={video_bg} type="video/mp4"></source>
+      </video>
+
+      <div className="container h-100">
+        <div className="d-flex h-100 text-center align-items-center">
+          <div className="w-100 text-white">
+            <h1>Tu futura casa es inteligente</h1>
+          </div>
         </div>
       </div>
     </header>
