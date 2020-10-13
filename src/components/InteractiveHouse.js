c5d8ad889ed6a290984a21da9d083eb79887924c
oyuFuentes <oyu.fuentes@gmail.com>
2020-10-12 21:39:31 -0500

    event click image

--- src/components/InteractiveHouse.js
+++ src/components/InteractiveHouse.js
@@ -4,40 +4,66 @@ import cil_couch from '../assets/images/interactive_house/cil_couch.png';
 import cil_door from '../assets/images/interactive_house/cil_door.png';
 import cil_garage from '../assets/images/interactive_house/cil_garage.png';
 import cil_tv from '../assets/images/interactive_house/cil_tv.png';
-import house_int1 from '../assets/images/interactive_house/exer-romero-render-night-final 1.png';
+//import night from '../assets/images/interactive_house/night.png';
+//import ligth from '../assets/images/interactive_house/ligth.png';
+
+const night = require('../assets/images/interactive_house/night.png');
+const ligth = require('../assets/images/interactive_house/ligth.png');
 
 class InteractiveHouse extends Component {
+  
+  state = {
+    night_active: true
+  }
+  toggleImage = () => {
+    this.setState(state => ({ night_active: !state.night_active }))
+  }
+  getImageName = () => this.state.night_active ? night : ligth
   render() {
+    const imageName = this.getImageName();
     return (
-      <div className="interactive-sect">
+      <div className="interactive-sect d-flex align-items-center">
         <div className="container">
-          <div className="col-md-12"><p className="TextInteractiveOne">Bienvenido a casa</p></div>
-          <div className="row">
+          <div className="row w-100"><p className="TextInteractiveOne">Bienvenido a casa</p></div>
+          <div className="row ">
             <div className="col-md-5">
               <div className="row">
                 <div className="col-md-12">
-                 <p className="TextInteractiveTwo">Disfruta la casa inteligente de tus sueños</p> 
+                  <p className="TextInteractiveTwo">Disfruta la casa inteligente de tus sueños</p>
                 </div>
                 <div className="col-md-12">
-                  <img src={cil_couch} alt="couch" />
+                  <p className="SpacingIconInteractive">
+                    <img className="IconImageIntereactive" src={cil_couch} alt="couch" />
+                  </p>
                 </div>
                 <div className="col-md-12">
-                  <img src={cil_bed} alt="bed" />
+                  <p className="SpacingIconInteractive">
+                    <img className="IconImageIntereactive" src={cil_bed} alt="bed" />
+                  </p>
                 </div>
                 <div className="col-md-12">
-                  <img src={cil_door} alt="door" />
+                  <p className="SpacingIconInteractive">
+                    <img className="IconImageIntereactive" src={cil_door} alt="door" />
+                  </p>
                 </div>
                 <div className="col-md-12">
-                  <img src={cil_tv} alt="tv" />
+                  <p className="SpacingIconInteractive">
+                    <img className="IconImageIntereactive" src={cil_tv} alt="tv" />
+                  </p>
                 </div>
                 <div className="col-md-12">
-                  <img src={cil_garage} alt="garage" />
+                  <p className="SpacingIconInteractive">
+                    <img className="IconImageIntereactive" src={cil_garage} alt="garage" />
+                  </p>
                 </div>
               </div>
             </div>
             <div className="col-md-7">
-              <img src={house_int1} alt="houseInt" />
+              <img src={imageName} alt="houseInt" />
             </div>
+          </div>
+          <div className="row">
+            <button type="button" class="btn btn-lg btn-info" onClick={this.toggleImage}>Info</button>
           </div>
         </div>
       </div>
